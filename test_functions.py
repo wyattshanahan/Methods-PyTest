1950099cee48089248286fb79e31c97b48820d42
bladn <bladner0717@gmail.com>
2022-11-29 17:31:13 -0600

    tests for greetUser()

--- test_functions.py
+++ test_functions.py
@@ -123,7 +123,7 @@ def greetUser(first, middle, last):
     print("Glad to have you!")
 # tests for correct output
 def test_greetUser(capsys):
-    greetUser("John", "Robert", "Doe")
+    greetUser("John", "Robert", "Doe") 
 
     captured_stdout, captured_stderr = capsys.readouterr()
     assert captured_stdout == "Hello!\nWelcome to the program John Robert Doe\nGlad to have you!\n"
