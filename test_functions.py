1e9cb76b38be401913544368aca8b3d2958e0db6
bladn <bladner0717@gmail.com>
2022-11-29 11:47:15 -0600

    added back deleted stuff

--- test_functions.py
+++ test_functions.py
@@ -1,5 +1,7 @@
 import pytest
 import math
+#this py file contains the pytest functions and the uncorrected original functions
+
 
 ## opens a file in read mode
 ## filename received as a parameter
@@ -26,6 +28,12 @@ def test_numbers_fail():
 
 #should throw a zero division error
 def test_numbers_zero():
+    assert numbers(10,0) == 0
+
+# tests the function using an integer and a float, should return an integer
+def test_numbers_1flt():
+    assert numbers(5,5.0) == 1.0
+
     with pytest.raises(ZeroDivisionError):
         numbers(1,0)
 def dist(x1, y1, x2, y2):
@@ -34,6 +42,28 @@ def dist(x1, y1, x2, y2):
 
     return dist
 
+#testing dist, should pass and return a long decimal using only integers
+def test_dist_1():
+    assert dist(1,2,3,4) == 2.8284271247461903
+#testing dist to ensure it can return a distance of zero
+def test_dist_2():
+    assert dist(4,2,4,2) == 0.0
+#testing dist, should return a float with a decimal of zero
+def test_dist_3():
+    assert (dist(1, 4, 2, 4)) == 1.0
+#testing dist to ensure it can handle a mix of floats and integers
+def test_dist_4():
+    assert (dist(1.,2.0,3.0,4.)) == 2.8284271247461903
+#testing dist with only floats
+def test_dist_4():
+    assert (dist(1.,2.0,3.0,4.)) == 2.8284271247461903
+#testing dist with a number stored as a string, this test should fail with a typeError
+def test_dist_5():
+    assert (dist("1",2,3,4)) == 2.8284271247461903
+#testing dist with an input of variables storing integers
+def test_dist_6():
+    x1,x2,y1,y2 = 1,2,3,4
+    assert (dist(x1,x2,y1,y2)) == 2.8284271247461903
 ## takes in a string -- reverses it
 ## then compares the two
 def isPalindrome(temp):
@@ -44,6 +74,28 @@ def isPalindrome(temp):
 
     else:
         return False
+
+#tests a palindrome in all lowercase characters
+def test_palin_1():
+    assert (isPalindrome("racecar")) == True
+#tests a palindrome with one capital letter, should fail without converting letters to the same case
+def test_palin_2():
+    assert(isPalindrome("Racecar")) == True
+#tests a palindrome in all uppercase letters
+def test_palin_3():
+    assert(isPalindrome("RACECAR")) == True
+#tests a multiword string that is a palindrome
+def test_palin_4():
+    assert(isPalindrome("racecar racecar")) == True
+#tests an integer that is a palindrome, should fail as it is not iterable
+def test_palin_5():
+    assert(isPalindrome(616)) == True
+#tests a float that is a palindrome, should fail as it is not iterable
+def test_palin_6():
+    assert (isPalindrome(616.616)) == True
+#tests an integer that is converted to a string, should be successful
+def test_palin_7():
+    assert (isPalindrome(str(616.616))) == True
 
 ## has input to receive two numbers
 ## divides the two, then outputs the result
@@ -69,21 +121,7 @@ def test_sq():
 def test_sq_is_float():
     assert type(sq(4)) == float
 
-def test_sq_fail():
-    assert sq(4) == 1
-
 ## grabs user's name
 ## greets them by their entire name
 ## names should be strings
 def greetUser(first, middle, last):
-    print("Hello!")
-    print("Welcome to the program", first, middle, last)
-    print("Glad to have you!")
-
-## takes in a Python list
-## attempts to display the item at the index provided
-def displayItem(numbers, index):
-    print("Your item at", index, "index is", numbers[index])
-
-# comment
-
